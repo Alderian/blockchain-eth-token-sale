86230abbf91061727b334167de5c82721eefb9a7
Alderian Annatar <53636+Alderian@users.noreply.github.com>
2022-06-05 19:25:35 -0300

    Mint tokens when selling

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,10 +1,13 @@
 pragma solidity 0.6.1;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract MyToken is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) ERC20Detailed("StarDucks Cappucino Token", "CAPPU", 0) public {
-        _mint(msg.sender, initialSupply);
+contract MyToken is ERC20Mintable, ERC20Detailed {
+    constructor()
+        public
+        ERC20Mintable()
+        ERC20Detailed("StarBuck Capuccino Token", "CAPPU", 0)
+    {
     }
 }
