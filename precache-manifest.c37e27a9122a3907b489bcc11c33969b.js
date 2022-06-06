5dcb8f93a1415f4e703f433015e8a77bd4c2207f
Oscar ALVAREZ <oalvarez@bancoindustrial.com.ar>
2022-06-05 22:37:38 -0300

    Publish on github

---
+++ precache-manifest.c37e27a9122a3907b489bcc11c33969b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d254153ccdbcfdc863a94f24e4e838f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265aff54a20a5d350a94",
+    "url": "/static/css/main.d590f0eb.chunk.css"
+  },
+  {
+    "revision": "7e815d4317290c0f5015",
+    "url": "/static/js/2.02f0523b.chunk.js"
+  },
+  {
+    "revision": "265aff54a20a5d350a94",
+    "url": "/static/js/main.12ac6bbe.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
